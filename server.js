f355edba97925a32c6749b5bad09c2701915680f
Prishan_105546 <prishan.niwanjaya@axiatadigitallabs.com>
2025-02-01 15:40:17 +0530

    Changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 8888;
+const PORT = 8080;
 
 app.use(express.static(path.join(__dirname, '')));
 
